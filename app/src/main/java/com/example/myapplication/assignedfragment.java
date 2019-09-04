0e9fb03bb907c54bba87251d4038924640e39276
harsh gandhi <harshgandhi043@gmail.com>
2019-09-05 00:26:29 +0530

    Added dynamic Content for DDA users

--- app/src/main/java/com/example/myapplication/assignedfragment.java
+++ app/src/main/java/com/example/myapplication/assignedfragment.java
@@ -1,20 +1,117 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class assignedfragment extends Fragment {
+
+    private static final String TAG = "assignedfragment";
+    private ArrayList<String> Id;
+    private ArrayList<String> Date;
+    private ArrayList<String> Time;
+    private ArrayList<String> Address;
+    private DdapendingAdapter ddaassignedAdapter;
+    private String urlget = "http://13.235.100.235:8000/api/locations/dda/assigned";
+    private String dda;
+    private String token;
+    private String villagename;
+    private String blockname;
+    private String district;
+    private String state;
+
+    public assignedfragment() {
+        Id = new ArrayList<String>(3);
+        Date = new ArrayList<String>(3);
+        Time = new ArrayList<String>(3);
+        Address = new ArrayList<String>(3);
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_assignedfragment, container, false);
+        View view = inflater.inflate(R.layout.fragment_ongoing,container,false);
+        ddaassignedAdapter = new DdapendingAdapter(getActivity(),Id,Date,Time,Address);
+        RecyclerView review = view.findViewById(R.id.recyclerViewongoing);
+        review.setAdapter(ddaassignedAdapter);
+        review.setLayoutManager( new LinearLayoutManager(getActivity()));
+
+        SharedPreferences preferences = getActivity().getSharedPreferences("tokenFile", Context.MODE_PRIVATE);
+        token = preferences.getString("token","");
+        Log.d(TAG, "onCreateView: "+token);
+
+        Log.d(TAG, "onCreateView: inflated fragment_ongoing");
+        final RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+
+        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, urlget, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(String.valueOf(response));
+                    JSONArray jsonArray = jsonObject.getJSONArray("results");
+                    for(int i=0;i<jsonArray.length();i++){
+                        JSONObject c = jsonArray.getJSONObject(i);
+                        dda = c.getString("dda");
+                        villagename = c.getString("village_name");
+                        blockname = c.getString("block_name");
+                        district = c.getString("district");
+                        state = c.getString("state");
+                        if(dda!=null && !dda.isEmpty()){
+                            Id.add(c.getString("id"));
+                            Date.add(c.getString("acq_date"));
+                            Time.add(c.getString("acq_time"));
+                            Address.add(villagename+","+blockname+","+district+","+state);
+                        }else {
+                            Log.d(TAG, "onResponse: some error in if");
+                        }
+                        ddaassignedAdapter.notifyDataSetChanged();
+                    }
+                }catch (JSONException e){
+                    Log.e(TAG, "onResponse: " + e.getLocalizedMessage());
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "onErrorResponse: " + error );
+            }
+        }) {
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> map = new HashMap<>();
+                map.put("Authorization", "Token " + token);
+                return map;
+            }
+        };
+
+        requestQueue.add(jsonObjectRequest);
+        return view;
     }
 }
--- app/src/main/java/com/example/myapplication/notassignedfragment.java
+++ app/src/main/java/com/example/myapplication/notassignedfragment.java
@@ -1,5 +1,7 @@
 package com.example.myapplication;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,22 +13,43 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class notassignedfragment extends Fragment {
 
     private static final String TAG = "notassignedfragment";
-    private ArrayList<String>locationid;
-    private ArrayList<String>latitude;
-    private ArrayList<String>longitude;
-    private ArrayList<String> address;
+    private ArrayList<String>Id;
+    private ArrayList<String>Date;
+    private ArrayList<String>Time;
+    private ArrayList<String> Address;
     private DdapendingAdapter notassignedDdapendingadapter;
+    private String urlget = "http://13.235.100.235:8000/api/locations/dda/unassigned";
+    private String villagename;
+    private String blockname;
+    private String district;
+    private String state;
+    private String token;
+    private String dda;
 
     public notassignedfragment(){
-        locationid = new ArrayList<String>(3);
-        latitude = new ArrayList<String>(3);
-        longitude = new ArrayList<String>(3);
-        address = new ArrayList<String>(3);
+        Id = new ArrayList<String>(3);
+        Date = new ArrayList<String>(3);
+        Time = new ArrayList<String>(3);
+        Address = new ArrayList<String>(3);
     }
 
     @Override
@@ -35,14 +58,62 @@ public class notassignedfragment extends Fragment {
         // Inflate the layout for this fragment
         Log.d(TAG, "onCreateView: in notassignedfragment");
         View view = inflater.inflate(R.layout.fragment_notassignedfragment, container, false);
-            locationid.add("L Id:");
-            latitude.add("Latitude:");
-            longitude.add("Longitude:");
-            address.add("Address:");
-        notassignedDdapendingadapter = new DdapendingAdapter(getActivity(),locationid,latitude,longitude, address);
+        notassignedDdapendingadapter = new DdapendingAdapter(getActivity(),Id,Date,Time, Address);
         RecyclerView notassignedreview = view.findViewById(R.id.recyclerViewnotassigned);
         notassignedreview.setAdapter(notassignedDdapendingadapter);
         notassignedreview.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        SharedPreferences preferences = getActivity().getSharedPreferences("tokenFile", Context.MODE_PRIVATE);
+        token = preferences.getString("token","");
+        Log.d(TAG, "onCreateView: "+token);
+
+        final RequestQueue unassignedrequestqueue = Volley.newRequestQueue(getActivity());
+
+        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, urlget, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(String.valueOf(response));
+                    JSONArray jsonArray = jsonObject.getJSONArray("results");
+                    for(int i=0;i<jsonArray.length();i++){
+                        JSONObject c = jsonArray.getJSONObject(i);
+                        dda = c.getString("dda");
+                        villagename = c.getString("village_name");
+                        blockname = c.getString("block_name");
+                        district = c.getString("district");
+                        state = c.getString("state");
+                        if(dda!=null && !dda.isEmpty()){
+                            Id.add(c.getString("id"));
+                            Date.add(c.getString("acq_date"));
+                            Time.add(c.getString("acq_time"));
+                            Address.add(villagename+","+blockname+","+district+","+state);
+                        }else {
+                            Log.d(TAG, "onResponse: some error in if");
+                        }
+                        notassignedDdapendingadapter.notifyDataSetChanged();
+                    }
+                }catch (JSONException e){
+                    Log.e(TAG, "onResponse: " + e.getLocalizedMessage());
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "onErrorResponse: " + error );
+            }
+        }) {
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> map = new HashMap<>();
+                map.put("Authorization", "Token " + token);
+                return map;
+            }
+        };
+
+        unassignedrequestqueue.add(jsonObjectRequest);
+
         return view;
     }
 }
