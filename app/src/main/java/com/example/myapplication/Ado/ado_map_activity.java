99c8793ad4978e65e263358715a32084e441d900
manav <manavmaini11@gmail.com>
2019-09-07 19:55:05 +0700

    ado on click listener on list and map activity and user name in header

--- app/src/main/java/com/example/myapplication/Ado/ado_map_activity.java
+++ app/src/main/java/com/example/myapplication/Ado/ado_map_activity.java
@@ -1,70 +1,43 @@
-package com.example.myapplication.Admin;
+package com.example.myapplication.Ado;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.FragmentManager;
-
+import com.example.myapplication.Admin.map_fragemnt;
 import com.example.myapplication.R;
-import com.example.myapplication.login_activity;
-import com.google.android.material.navigation.NavigationView;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class AdminActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class ado_map_activity extends AppCompatActivity {
 
-    private final String TAG = "AdminActivity";
     private final String ACCESS_FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private final String ACCESS_COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
-    private final String READ_EXTERNAL_STORAGE = Manifest.permission.READ_EXTERNAL_STORAGE;
-    private final String WRITE_EXTERNAL_STORAGE = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
     private final int RESULT_CODE = 786;
-    private NavigationView navigationView;
-
-
+    private GoogleMap map=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        navigationView = findViewById(R.id.nav_view);
-        if (getPermission()) {
-            navigationView.setCheckedItem(R.id.nav_home);
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, new map_fragemnt()).commit();
-        }
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-        navigationView.setNavigationItemSelectedListener(this);
+        setContentView(R.layout.activity_ado_map_activity);
     }
 
     private boolean getPermission() {
@@ -77,16 +50,6 @@ public class AdminActivity extends AppCompatActivity
         if (ContextCompat.checkSelfPermission(this.getApplicationContext(), ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
             Permission.add(ACCESS_COARSE_LOCATION);
-        }
-
-        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            Permission.add(READ_EXTERNAL_STORAGE);
-        }
-
-        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            Permission.add(WRITE_EXTERNAL_STORAGE);
         }
 
         if (!Permission.isEmpty()) {
@@ -113,8 +76,17 @@ public class AdminActivity extends AppCompatActivity
             }
 
             if (deniedCount == 0) {
-                navigationView.setCheckedItem(R.id.nav_home);
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, new map_fragemnt()).commit();
+                SupportMapFragment mapFragment = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.ado_map));
+
+                mapFragment.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(GoogleMap googleMap) {
+                        map=googleMap;
+
+
+                    }
+                });
+
 
             } else {
                 for (Map.Entry<String, Integer> entry : permissionResults.entrySet()) {
@@ -181,83 +153,9 @@ public class AdminActivity extends AppCompatActivity
         return alert;
     }
 
-    private void showMap() {
-        Log.d(TAG, "showMap: getsupport");
-        FragmentManager mfragmentmanager = getSupportFragmentManager();
-        Log.d(TAG, "showMap: getsupport" + mfragmentmanager);
-        mfragmentmanager.beginTransaction().replace(R.id.container, new map_fragemnt()).commit();
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_logout) {
-            SharedPreferences.Editor editor = getSharedPreferences("tokenFile", MODE_PRIVATE).edit();
-            editor.clear();
-            editor.commit();
-            Intent intent = new Intent(AdminActivity.this, login_activity.class);
-            startActivity(intent);
-            finish();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_upload) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, new upload_fragment()).commit();
-
-        } else if (id == R.id.nav_ddo) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, new ddo_fragment()).commit();
 
 
-        } else if (id == R.id.nav_ado) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, new ado_fragment()).commit();
 
 
-        } else if (id == R.id.nav_location) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, new location_fragment()).commit();
+}
 
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        } else if (id == R.id.nav_home) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, new map_fragemnt()).commit();
-        }
-
-
-            DrawerLayout drawer = findViewById(R.id.drawer_layout);
-            drawer.closeDrawer(GravityCompat.START);
-            return true;
-        }
-    }
