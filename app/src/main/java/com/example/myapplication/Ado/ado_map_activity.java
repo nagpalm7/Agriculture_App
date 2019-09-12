5c7f912fb8c9ae9199a226268e3cc03a8b2847df
“sahilbhardwaj20” <sahilbhardwaj20@gmail.com>
2019-09-12 17:02:14 +0530

    ado functionality

--- app/src/main/java/com/example/myapplication/Ado/ado_map_activity.java
+++ app/src/main/java/com/example/myapplication/Ado/ado_map_activity.java
@@ -7,23 +7,35 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.myapplication.Admin.map_fragemnt;
 import com.example.myapplication.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -34,193 +46,185 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
-public class ado_map_activity extends AppCompatActivity {
+public class ado_map_activity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private final String ACCESS_FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
-    private final String ACCESS_COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private double longitude;
     private double latitude;
-    private Boolean showmap;
 
     private final int RESULT_CODE = 786;
-    private GoogleMap map=null;
+    private GoogleMap mMap=null;
     private final String TAG= "ado_map_activity";
+    private boolean isGPS = false;
+    private boolean isContinue = false;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+    private StringBuilder stringBuilder;
+    private double cLatitude = 0.0, cLongitude = 0.0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ado_map_activity);
-        Intent intent = getIntent();
-        longitude = Double.parseDouble(intent.getStringExtra("longitude"));
-        latitude = Double.parseDouble(intent.getStringExtra("latitude"));
-        Log.d(TAG, "onCreate: "+latitude+" "+longitude);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.ado_map);
+        mapFragment.getMapAsync(this);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(10 * 1000); // 10 seconds
+        locationRequest.setFastestInterval(5 * 1000); // 5 seconds
 
 
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                   }
+                for (Location location : locationResult.getLocations()) {
+                    if (location != null) {
+                        cLatitude = location.getLatitude();
+                        cLongitude = location.getLongitude();
+                        if (!isContinue) {
 
-        if(getPermission()){
-            SupportMapFragment mapFragment = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.ado_map));
-
-            mapFragment.getMapAsync(new OnMapReadyCallback() {
-                @Override
-                public void onMapReady(GoogleMap googleMap) {
-                    Log.d("inside onMapReady", "onMapReady: ");
-                    map=googleMap;
-
-                    //get latlong for corners for specified city
-
-                    LatLng one = new LatLng(7.798000, 68.14712);
-                    LatLng two = new LatLng(37.090000, 97.34466);
-
-                    LatLngBounds.Builder builder = new LatLngBounds.Builder();
-
-                    //add them to builder
-                    builder.include(one);
-                    builder.include(two);
-
-                    LatLngBounds bounds = builder.build();
-
-                    //get width and height to current display screen
-                    int width = getResources().getDisplayMetrics().widthPixels;
-                    int height = getResources().getDisplayMetrics().heightPixels;
-
-                    // 20% padding
-                    int padding = (int) (width * 0.20);
-
-                    //set latlong bounds
-                    map.setLatLngBoundsForCameraTarget(bounds);
-
-                    //move camera to fill the bound to screen
-                    map.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, width, height, padding));
-
-                    //set zoom to level to current so that you won't be able to zoom out viz. move outside bounds
-                    map.setMinZoomPreference(map.getCameraPosition().zoom);
-
-                    //marking the position
-                    map.addMarker(new MarkerOptions().position(new LatLng(latitude,longitude)).title("edar aaa").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
-
-                }
-
-
-
-            });
-        }
-
-
-
-
-    }
-
-    private boolean getPermission() {
-        List<String> Permission = new ArrayList();
-        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            Permission.add(ACCESS_FINE_LOCATION);
-        }
-
-        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            Permission.add(ACCESS_COARSE_LOCATION);
-        }
-
-        if (!Permission.isEmpty()) {
-            String[] permissions = Permission.toArray(new String[Permission.size()]);
-            ActivityCompat.requestPermissions(this, permissions, RESULT_CODE);
-            return false;
-        } else
-            return true;
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
-        if (requestCode == RESULT_CODE) {
-            HashMap<String, Integer> permissionResults = new HashMap<>();
-            int deniedCount = 0;
-
-            for (int i = 0; i < grantResults.length; i++) {
-                if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
-                    permissionResults.put(permissions[i], grantResults[i]);
-                    deniedCount++;
-                }
-            }
-
-            if (deniedCount == 0) {
-            } else {
-                for (Map.Entry<String, Integer> entry : permissionResults.entrySet()) {
-                    String permName = entry.getKey();
-                    int permResult = entry.getValue();
-
-                    if (ActivityCompat.shouldShowRequestPermissionRationale(this, permName)) {
-                        showDialog("", "This app needs location and files permissions to work without any problems.",
-                                "Yes, Grant permissions",
-                                new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialogInterface, int i) {
-                                        dialogInterface.dismiss();
-                                        getPermission();
-                                    }
-                                },
-                                "No, Exit app",
-                                new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialogInterface, int i) {
-                                        dialogInterface.dismiss();
-                                        finish();
-                                    }
-                                }, false);
-                    } else {
-                        showDialog("",
-                                "You have denied some permissions. Allow all the permissions at [Setting] > [Permissions]",
-                                "Go to Settings",
-                                new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialogInterface, int i) {
-                                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                                                Uri.fromParts("package", getPackageName(), null));
-                                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                        startActivity(intent);
-                                        finish();
-                                    }
-                                }, "No, Exit App",
-                                new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialogInterface, int i) {
-                                        dialogInterface.dismiss();
-                                        finish();
-                                    }
-                                }, false);
-                        break;
+                        } else {
+                            stringBuilder.append(cLatitude);
+                            stringBuilder.append("-");
+                            stringBuilder.append(cLongitude);
+                            stringBuilder.append("\n\n");
+                        }
+                        if (!isContinue && mFusedLocationClient != null) {
+                            mFusedLocationClient.removeLocationUpdates(locationCallback);
+                        }
                     }
                 }
             }
-        }
+        };
+
+    }
+    LatLng latLng;
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        Intent intent = getIntent();
+        longitude = Double.parseDouble(intent.getStringExtra("longitude"));
+        latitude = Double.parseDouble(intent.getStringExtra("latitude"));
+        Log.d(TAG, "onCreate: " + latitude + " " + longitude);
+        latLng = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(latLng).title(latitude + " , " + longitude));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,14f));
+        drawGeofence();
+    }
+    private Circle geoFenceLimits;
+    private static final float GEOFENCE_RADIUS = 500.0f; // in meters
+
+    // Draw Geofence circle on GoogleMap
+    private void drawGeofence() {
+
+        if ( geoFenceLimits != null )
+            geoFenceLimits.remove();
+
+        CircleOptions circleOptions = new CircleOptions()
+                .center(latLng)
+                .strokeColor(Color.argb(50, 70,70,70))
+                .fillColor( Color.argb(100, 150,150,150) )
+                .radius( GEOFENCE_RADIUS );
+        geoFenceLimits = mMap.addCircle( circleOptions );
     }
 
-    private AlertDialog showDialog(String title, String msg, String positiveLabel, DialogInterface.OnClickListener positiveOnclick,
-                                   String negativeLabel, DialogInterface.OnClickListener negativeOnclick,
-                                   boolean isCancelable) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(title);
-        builder.setMessage(msg);
-        builder.setPositiveButton(positiveLabel, positiveOnclick);
-        builder.setNegativeButton(negativeLabel, negativeOnclick);
-        builder.setCancelable(isCancelable);
-        AlertDialog alert = builder.create();
-        alert.show();
-        return alert;
-    }
 
 
     public void onClickNavigation(View view) {
-        String uri = String.format(Locale.ENGLISH, "http://maps.google.com/maps?daddr=%f,%f (%s)", latitude, longitude, "Where the party is at");
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-        intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
-        startActivity(intent);
+        {new GpsUtils(ado_map_activity.this).turnGPSOn(isGPSEnable -> {
+            // turn on GPS
+            isGPS = isGPSEnable;
+        });
+
+            if (!isGPS) {
+                Toast.makeText(ado_map_activity.this, "Please turn on GPS", Toast.LENGTH_SHORT).show();
+                return;
+            }else {
+                isContinue = false;
+                getLocation();
+            }
+            if(cLatitude!=0.0&&cLongitude!=0.0) {
+                Uri gmmIntentUri = Uri.parse("google.navigation:q=" + latLng.latitude + "," + latLng.longitude);
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+
+                }
+            }
+        }
     }
 
     public void onClickCheckIn(View view) {
         //check in code
     }
-}
 
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(ado_map_activity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(ado_map_activity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(ado_map_activity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},1000);
+
+        } else {
+            if (isContinue) {
+                mFusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
+            } else {
+                mFusedLocationClient.getLastLocation().addOnSuccessListener(ado_map_activity.this, location -> {
+                    if (location != null) {
+                        cLatitude = location.getLatitude();
+                        cLongitude = location.getLongitude();
+                    } else {
+                        mFusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
+                    }
+                });
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1000: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    if (isContinue) {
+                        mFusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
+                    } else {
+                        mFusedLocationClient.getLastLocation().addOnSuccessListener(ado_map_activity.this, location -> {
+                            if (location != null) {
+                                cLatitude = location.getLatitude();
+                                cLongitude = location.getLongitude();
+                            } else {
+                                mFusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
+                            }
+                        });
+                    }
+                } else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == 1001) {
+                isGPS = true; // flag maintain before get location
+            }
+        }
+    }
+}
