6d2066ad5f1a3478818b9f54a4f3f6ccebc3e185
manav <manavmaini11@gmail.com>
2019-09-19 00:47:30 +0530

    layout fixed

---
+++ app/src/main/java/com/example/myapplication/Admin/EditActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}
